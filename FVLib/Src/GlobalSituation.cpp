ba933965dc7f9519f29e80bf8bbea9f222f2beaf
Leonid <derbenevleo@gmail.com>
2024-05-30 22:27:52 +0500

    Добавил время записи короткого плана в FVState. Изменил вывод new_plan

--- FVLib/Src/GlobalSituation.cpp
+++ FVLib/Src/GlobalSituation.cpp
@@ -5,8 +5,9 @@ void AEtherInfo::broadcastState(const Plane& plane)
 	states[plane.name].plane = plane;
 }
 
-void AEtherInfo::broadcastPlan(const string& name, const vector<Point>& shortPlan)
+void AEtherInfo::broadcastPlan(const string& name,double time, const vector<Point>& shortPlan)
 {
 	states[name].shortPlan = shortPlan;
+	states[name].translationTime = time;
 	//states[name].shortPlan = vector<Point>();
 }
